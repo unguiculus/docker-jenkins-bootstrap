92f9328b078c381f7f1d2bbdc6c607c3b833b6c4
Reinhard Nägele <unguiculus@gmail.com>
2017-05-05 23:43:44 +0200

    Use multi-branch pipeline for seed job

--- docker/examples/jobDsl.groovy
+++ docker/examples/jobDsl.groovy
@@ -1,33 +1,23 @@
-freeStyleJob('__seed-job') {
-    logRotator {
-        numToKeep(30)
+multibranchPipelineJob('__seed-job') {
+    branchSources {
+        github {
+            repoOwner('unguiculus')
+            repository('job-dsl-sample')
+            buildForkPRHead(false)
+            buildForkPRMerge(true)
+            buildOriginBranch(true)
+            buildOriginBranchWithPR(false)
+            buildOriginPRHead(false)
+            buildOriginPRMerge(true)
+        }
+    }
+    orphanedItemStrategy {
+        discardOldItems {
+            daysToKeep(0)
+            numToKeep(0)
+        }
     }
     triggers {
-        scm('* * * * *')
-    }
-    wrappers {
-        timestamps()
-    }
-    scm {
-        git {
-            remote {
-                url('https://github.com/unguiculus/job-dsl-sample.git')
-                name('origin')
-            }
-            branch('origin/master')
-            extensions {
-                localBranch('master')
-            }
-        }
-    }
-    steps {
-        dsl {
-            additionalClasspath('src/main/groovy')
-            external('jobs/jobs.groovy')
-            ignoreExisting(false)
-            lookupStrategy('JENKINS_ROOT')
-            removeAction('DELETE')
-            removeViewAction('DELETE')
-        }
+        periodic(1)
     }
 }
