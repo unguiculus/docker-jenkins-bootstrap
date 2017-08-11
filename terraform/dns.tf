6b64bdb9cc7a7b276b3d074632eb9626673ae85b
Reinhard Nägele <unguiculus@gmail.com>
2017-08-11 08:42:10 +0200

    Update domain setup to fix SERVFAIL error

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -1,4 +1,11 @@
 resource "digitalocean_domain" "domain" {
-  name = "jenkins.unguiculus.io"
-  ip_address = "${digitalocean_droplet.jenkins.ipv4_address}"
+    name = "unguiculus.io"
+    ip_address = "${digitalocean_droplet.jenkins.ipv4_address}"
+}
+
+resource "digitalocean_record" "record" {
+    domain = "${digitalocean_domain.domain.name}"
+    type = "A"
+    name = "jenkins"
+    value = "${digitalocean_droplet.jenkins.ipv4_address}"
 }
