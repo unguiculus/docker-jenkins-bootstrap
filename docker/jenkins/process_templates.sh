019c50ca0db27c3544b0c45f3ec25b8c64446314
Reinhard Nägele <unguiculus@gmail.com>
2017-09-12 21:08:36 +0200

    Give master image its own version

--- docker/jenkins/process_templates.sh
+++ docker/jenkins/process_templates.sh
@@ -10,22 +10,23 @@ function main() {
 
     . ./templates.env
 
-    echo "JENKINS_MASTER_VERSION: $JENKINS_MASTER_VERSION"
-    echo "JENKINS_SLAVE_VERSION: $JENKINS_SLAVE_VERSION"
+    echo "JENKINS_VERSION: $JENKINS_VERSION"
+    echo "MASTER_IMAGE_VERSION: $MASTER_IMAGE_VERSION"
+    echo "SLAVE_IMAGE_VERSION: $SLAVE_IMAGE_VERSION"
     echo "JENKINS_HOST: $JENKINS_HOST"
     echo "LETSENCRYPT_EMAIL: $LETSENCRYPT_EMAIL"
 
     echo 'Generating docker-compose.yml from template: ./templates/docker-compose.yml'
 
-    sed "s/@JENKINS_MASTER_VERSION@/$JENKINS_MASTER_VERSION/g" ./templates/docker-compose.yml \
-        | sed "s/@JENKINS_SLAVE_VERSION@/$JENKINS_SLAVE_VERSION/g" \
+    sed "s/@MASTER_IMAGE_VERSION@/$MASTER_IMAGE_VERSION/g" ./templates/docker-compose.yml \
+        | sed "s/@SLAVE_IMAGE_VERSION@/$SLAVE_IMAGE_VERSION/g" \
         | sed "s/@JENKINS_HOST@/$JENKINS_HOST/g" \
         | sed "s/@LETSENCRYPT_EMAIL@/$LETSENCRYPT_EMAIL/g" \
         > docker-compose.yml
 
     echo 'Generating jenkins_master/Dockerfile from template: ./templates/Dockerfile'
 
-    sed "s/@JENKINS_MASTER_VERSION@/$JENKINS_MASTER_VERSION/g" ./templates/Dockerfile > jenkins_master/Dockerfile
+    sed "s/@JENKINS_VERSION@/$JENKINS_VERSION/g" ./templates/Dockerfile > jenkins_master/Dockerfile
 
     popd > /dev/null
 }
