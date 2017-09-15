b121d15efc1ad0008d14490bc4a0ff96e0df048f
Reinhard Nägele <unguiculus@gmail.com>
2017-09-15 11:32:19 +0200

    Use larger droplet (4GB)

--- terraform/droplet.tf
+++ terraform/droplet.tf
@@ -7,7 +7,7 @@ resource "digitalocean_droplet" "jenkins" {
   image = "ubuntu-16-04-x64"
   name = "jenkins"
   region = "${var.region}"
-  size = "2GB"
+  size = "4GB"
   ssh_keys = ["${digitalocean_ssh_key.ssh_key.id}"]
 }
 
