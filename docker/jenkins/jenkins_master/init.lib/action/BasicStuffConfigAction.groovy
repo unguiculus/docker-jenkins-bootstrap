1b5f890b754031027d9c3bc307afab6b86ac1cd2
Reinhard Nägele <unguiculus@gmail.com>
2018-04-11 17:28:25 +0200

    Fix disabling install and plugin wizard

--- docker/jenkins/jenkins_master/init.lib/action/BasicStuffConfigAction.groovy
+++ docker/jenkins/jenkins_master/init.lib/action/BasicStuffConfigAction.groovy
@@ -3,18 +3,11 @@ package action
 import groovy.transform.InheritConstructors
 import jenkins.model.Jenkins
 
-import java.nio.file.Path
-import java.nio.file.Paths
-
 @InheritConstructors
 class BasicStuffConfigAction extends ConfigAction {
 
     @Override
     void execute() {
-        def jenkinsHome = Paths.get(System.getenv('JENKINS_HOME'))
-        Path installStateFile = jenkinsHome.resolve('jenkins.install.UpgradeWizard.state')
-        installStateFile.text = '2.0'
-
         def instance = Jenkins.getInstance()
 
         instance.setNumExecutors(0)
--- docker/jenkins/process_templates.sh
+++ docker/jenkins/process_templates.sh
@@ -5,20 +5,23 @@ set -u
 set -o pipefail
 
 main() {
-    local scriptdir=$(dirname "$(readlink -f "$0")")
+    local scriptdir
+    scriptdir=$(dirname "$(readlink -f "$0")")
+
     pushd "$scriptdir" > /dev/null
 
     . ./templates.env
 
     echo "JENKINS_VERSION: $JENKINS_VERSION"
-    echo "MASTER_IMAGE_VERSION: $MASTER_IMAGE_VERSION"
+    echo "MASTER_BUILD_NUMBER: $MASTER_BUILD_NUMBER"
     echo "SLAVE_IMAGE_VERSION: $SLAVE_IMAGE_VERSION"
     echo "JENKINS_HOST: $JENKINS_HOST"
     echo "LETSENCRYPT_EMAIL: $LETSENCRYPT_EMAIL"
 
     echo 'Generating docker-compose.yml from template: ./templates/docker-compose.yml'
 
-    sed "s/@MASTER_IMAGE_VERSION@/$MASTER_IMAGE_VERSION/g" ./templates/docker-compose.yml \
+    sed "s/@MASTER_BUILD_NUMBER@/$MASTER_BUILD_NUMBER/g" ./templates/docker-compose.yml \
+        | sed "s/@JENKINS_VERSION@/$JENKINS_VERSION/g" \
         | sed "s/@SLAVE_IMAGE_VERSION@/$SLAVE_IMAGE_VERSION/g" \
         | sed "s/@JENKINS_HOST@/$JENKINS_HOST/g" \
         | sed "s/@LETSENCRYPT_EMAIL@/$LETSENCRYPT_EMAIL/g" \
